ebf14d847d4bb001fb1fa7b162dcb6d57f15e386
matarsh11 <54373365+matarsh11@users.noreply.github.com>
2019-09-03 12:54:00 -0500

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+/*Saleh Almatar
+C++ Fall 2019
+Date: 08 ,27,2019
+Lab#: Lab1
+Lab dessricptio: checkerboard pattern made of starsand blanks
+*/
+#include <iostream>
+int main()
+{
+	std::cout << "  *   *   *   *   *\n";
+
+	std::cout << "*   *   *   *   *\n";
+
+	std::cout << "  *   *   *   *   *\n";
+
+	std::cout << "*   *   *   *   *\n";
+
+	std::cout << "  *   *   *   *   *\n";
+
+	std::cout << "*   *   *   *   *\n";
+
+	std::cout << "  *   *   *   *   *\n";
+
+	std::cout << "*   *   *   *   *\n";
+}
